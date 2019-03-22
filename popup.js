d44f2c150594fd4f1bfacc85076b1c2e3d41eb2d
HiDoYa <hiroyagojo@gmail.com>
2019-03-22 09:46:10 -0700

    Can now find classes to save

--- popup.js
+++ popup.js
@@ -8,27 +8,28 @@ document.addEventListener('DOMContentLoaded', () => {
     ); 
     
 });
-
+var allClasses;
 window.addEventListener('DOMContentLoaded', (event) =>  {
     var getCalendar = document.getElementById('getCalendar');
     getCalendar.onclick = function() {
+
+        /// FIND WHICH CLASSES TO SAVE
         var currentDocument = new DOMParser().parseFromString(htmlCode, "text/html")
-        var allClasses = currentDocument.getElementsByClassName("classTitle");
+        allClasses = currentDocument.getElementsByClassName("classTitle");
         var classToSave = []
         
         for (var i = 0; i < allClasses.length; i++) {
-            console.log("running3");
 
-            var currentClass = allClasses[i].childNodes;
-            if (currentClass.length > 0) {
-                console.log("running2");
-
-                var statusIndicator = currentClass[0].getElementsByClassName("statusIndicator");
-                if (statusIndicator.innertext == "Registered") {
+            var currentClass = allClasses[i].nextElementSibling;
+            if (currentClass != undefined) {
+                // Only include registered classes (not waitlisted)
+                var statusIndicator = currentClass.getElementsByClassName("statusIndicator registered");
+                if (statusIndicator != undefined && statusIndicator.legnth != 0) {
+                    console.log(statusIndicator)
                     classToSave.push(i);
-                    console.log("running1");
                 }
             }
+        
         }
     }
 })
