86b3796285ad1769755c152ad252ef448c18d5d5
HiDoYa <hiroyagojo@gmail.com>
2019-03-22 14:27:46 -0700

    Bug: incorrect times showing up

--- popup.js
+++ popup.js
@@ -11,8 +11,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
 window.addEventListener('DOMContentLoaded', () =>  {
     // Set default value for html date inputs
-    document.getElementsByClassName('startDate')[0].value = new Date().toDateInputValue();
-    document.getElementsByClassName('endDate')[0].value = new Date().toDateInputValue();
+    var today = new Date();
+    var format = today.toISOString().split("T")[0];
+    document.getElementsByClassName('startDate')[0].value = format;
+    document.getElementsByClassName('endDate')[0].value = format;
 
     // Clicks
     var getCalendar = document.getElementById('getCalendar');
@@ -20,7 +22,7 @@ window.addEventListener('DOMContentLoaded', () =>  {
         /// RETRIEVE DATA FROM FORMS
         var formData = new FormData(document.querySelector('form'));
         var formItr = formData.entries()
-        var startTime = new Date(formItr.next().value[0]);
+        var startTime = new Date(formItr.next().value[1]);
         var endTime = new Date(formItr.next().value[1]);
         endTime.setHours(23,59,59);
 
@@ -51,7 +53,13 @@ window.addEventListener('DOMContentLoaded', () =>  {
         for (var i = 0; i < classToSave.length; i++) {
             var currentClass = classToSave[i].nextElementSibling;
             var infoGet = currentClass.getElementsByClassName("meeting");
-            var name = classToSave[i].innerText;
+
+            var nameArr = classToSave[i].innerText.split(' ');
+            while (nameArr.length != 2) {
+                nameArr.pop();
+            }
+            var name = nameArr.join('');
+            
 
             for (var j = 0; j < infoGet.length; j++){
                 counter++;
@@ -113,13 +121,13 @@ window.addEventListener('DOMContentLoaded', () =>  {
                     var shift = start - startTimeLoc.getDay();
                     startTimeLoc.setDate(startTimeLoc.getDate() + shift);
                 }
-                var beginning = startTimeLoc.toISOString();
+                var beginning = startTimeLoc;
                 beginning.setHours(timeStart[0], timeStart[1]);
-                classInfo.startTimeLoc.push(beginning);
+                classInfo.startTime.push(dateStrFormat(beginning.toISOString()));
 
-                var ending = startTimeLoc.toISOString();
+                var ending = startTimeLoc;
                 ending.setHours(timeEnd[0], timeEnd[1]);
-                classInfo.endTime.push(ending);
+                classInfo.endTime.push(dateStrFormat(ending.toISOString()));
 
                 classInfo.location.push(infoGet[j].children[3].innerText);
             }
@@ -129,45 +137,72 @@ window.addEventListener('DOMContentLoaded', () =>  {
         // ics format: https://tools.ietf.org/html/rfc5545
 
         // Heading
-        var BEGIN = "BEGIN:VCALENDAR";
-        var PRODID = "PRODID:-//Google Inc//Google Calendar 70.9054//EN";
-        var VERSION = "VERSION:2.0";
-        var CALSCALE = "CALSCALE:GREGORIAN";
-        var METHOD = "METHOD:PUBLISH";
-        var X_WR_CALNAME = "X-WR-CALNAME:Class";
-        var X_WR_TIMEZONE = "X-WR-TIMEZONE:America/Los_Angeles";
+        var final = heading();
 
         // Get the string to return
         for (var i = 0; i < counter; i++) {
             // Get and format the date
             var dateObj = new Date();
-            var current = dateObj.toISOString();
-            current.split(":").join("");
-            current.split("-").join("");
-
-            var until = endTime.toISOString();
-            until.split(":").join("");
-            until.split("-").join("");
+            var current = dateStrFormat(dateObj.toISOString());
+            var until = dateStrFormat(endTime.toISOString());
 
             // Get DTSTART, DTEND, endDate, location, summary, weekDays
-            var DTSTART = "DTSTART:" + classInfo.startTime[i];
-            var DTEND = "DTEND:" + classInfo.endTime[i];
-            var RRULE_UNTIL_BYDAY = "RRULE:FREQ=WEEKLY;" + "UNTIL=" + until + ';' + "BYDAY:" + classInfo.weekDay[i];
-            var DTSTAMP = "DTSTMP:" + current;
-            var UID = "UID:" + current + "@schedulebuilder";
-            var CREATED = "CREATED:" + current;
+            var BEGIN = "BEGIN:VEVENT";
+            var DTSTART = "DTSTART;TZID=America/Los_Angeles:" + classInfo.startTime[i] + "Z";
+            var DTEND = "DTEND;TZID=America/Los_Angeles:" + classInfo.endTime[i] + "Z";
+            var RRULE_UNTIL_BYDAY = "RRULE:FREQ=WEEKLY;" + "UNTIL=" + until + 'Z;' + "BYDAY:" + classInfo.weekDay[i];
+            var DTSTAMP = "DTSTMP:" + current + "Z";
+            var UID = "UID:" + current + Math.random() + "@schedulebuilder";
+            var CREATED = "CREATED:" + current + "Z";
             var DESCRIPTION = "DESCRIPTION:";
-            var LAST_MODIFIED = "LAST-MODIFIED:" + current;
+            var LAST_MODIFIED = "LAST-MODIFIED:" + current + "Z";
             var LOCATION = "LOCATION:" + classInfo.location[i];
             var SEQUENCE = "SEQUENCE:0"; 
             var STATUS = "STATUS:CONFIRMED";
             var SUMMARY = "SUMMARY:" + classInfo.name[i];
             var TRANSP = "TRANSP:OPAQUE";
-            var END = "END:EVENT";
+            var END = "END:VEVENT";
 
+            var strArr = [BEGIN, DTSTART, DTEND, RRULE_UNTIL_BYDAY, DTSTAMP, UID, CREATED, DESCRIPTION, LAST_MODIFIED, LOCATION, SEQUENCE, STATUS, SUMMARY, TRANSP, END];
+            final += '\n' + strArr.join('\n');
         }
+        final += '\n' + "END:VCALENDAR";
+
+        download(final, 'export.ics', 'ics')
     }
 })
+
+function heading () {
+    var str = [
+        "BEGIN:VCALENDAR",
+        "PRODID:-//Google Inc//Google Calendar 70.9054//EN",
+        "VERSION:2.0",
+        "CALSCALE:GREGORIAN",
+        "METHOD:PUBLISH",
+        "X-WR-CALNAME:Class",
+        "X-WR-TIMEZONE:America/Los_Angeles",
+        "BEGIN:VTIMEZONE",
+        "TZID:America/Los_Angeles",
+        "X-LIC-LOCATION:America/Los_Angeles",
+        "BEGIN:DAYLIGHT",
+        "TZOFFSETFROM:-0800",
+        "TZOFFSETTO:-0700",
+        "TZNAME:PDT",
+        "DTSTART:19700308T020000",
+        "RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=2SU",
+        "END:DAYLIGHT",
+        "BEGIN:STANDARD",
+        "TZOFFSETFROM:-0700",
+        "TZOFFSETTO:-0800",
+        "TZNAME:PST",
+        "DTSTART:19701101T020000",
+        "RRULE:FREQ=YEARLY;BYMONTH=11;BYDAY=1SU",
+        "END:STANDARD",
+        "END:VTIMEZONE"
+    ]
+
+    return str.join('\n');
+}
 
 function timeGet(time, str) {
     if (str == "AM") {
@@ -177,5 +212,29 @@ function timeGet(time, str) {
         var spTime = time.split(':');
         spTime[0] += 12;
         return spTime;
+    }
+}
+
+function dateStrFormat(date) {
+    date = date.split(":").join("");
+    date = date.split("-").join("");
+    return date = date.split(".")[0]
+}
+
+function download(data, filename, type) {
+    var file = new Blob([data], {type: type});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
     }
 }
